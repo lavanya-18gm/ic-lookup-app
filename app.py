7b05e1ae490d195a4e1a34d47373ffed59595648
Lavanya G M <lavanya18gm@gmail.com>
2025-08-01 19:16:54 +0530

    Updated app.py and added requirements.txt

--- app.py
+++ app.py
@@ -18,5 +18,7 @@ def index():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-print("Rendering index.html from:", __file__)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
+
